c9d74bf1af3328f646b023b9353b8c7f6cb18a21
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-17 18:08:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
   if (i === 1) {
     return "I am 1 strange loop.";
   } else {
-    return ("I am" + i + "strange loops");
+    return ("I am " + i + " strange loops");
   }
 }
 }
